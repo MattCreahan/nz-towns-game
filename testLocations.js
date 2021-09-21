a9b940813c1c8cf88a24bc59bb3daf6e7a6551b1
MattCreahan <matt@creahan.co.nz>
2021-09-21 14:20:03 +1200

    Added markers on the cities when the user types them in, and a counter for the total number of cities found

--- testLocations.js
+++ testLocations.js
@@ -1,25 +1,40 @@
 const towns = [{
-        Name: 'Auckland',
+        name: 'Auckland',
         lat: -36.8509,
         long: 174.7645,
         population: 1657000
     },
     {
-        Name: 'Christchurch',
+        name: 'Christchurch',
         lat: -43.5320,
         long: 172.6306,
         population: 381500
     }
 ];
 
+let numCorrect = 0;
+
 function checkInput() {
     const input = document.getElementById('townInput').value;
-    if (isValidTown(input)) {
-        console.log(`${input} is a valid town!`);
-        document.getElementById('townInput').value = '';
+    const townObject = towns.find(town => town.name.toLowerCase() === input.toLowerCase());
+    if (!(townObject==null)) {
+        clearInputBox();
+        addMarker(townObject);
+        incrementScore();
     }
 }
 
-function isValidTown(input) {
-    return towns.some(town => town['Name'].toLowerCase() === input.toLowerCase());
+function clearInputBox() {
+    document.getElementById('townInput').value = '';
+}
+
+function addMarker(townObject) {
+    const marker = new mapboxgl.Marker({
+        color: '#5E9E3E'
+    }).setLngLat([townObject.long, townObject.lat]);
+    marker.addTo(map);
+}
+
+function incrementScore() {
+    document.getElementById('score').innerHTML = `You have named ${++numCorrect} ${numCorrect > 1 ? 'towns/cities' : 'town/city'}!`;
 }
